830840f9fea499847e84f7376c08e3ea9ac7b754
Malekabo123 <malismail20@posta.pau.edu.tr>
2023-11-23 20:58:34 +0300

    commit

--- src/components/BgImage/BgImage.jsx
+++ src/components/BgImage/BgImage.jsx
@@ -1,11 +1,10 @@
 import "./BgImage.css";
-import myImg from "../../../public/foodiesfeed.com_healthy-foods.jpg";
 
 const BgImage = () => {
   return (
     <div id="stick_content">
       <div id="img_container">
-        <img src="/foodiesfeed.com_healthy-foods.jpg" id="bg_img"></img>
+        <img src="./foodiesfeed.com_healthy-foods.jpg" id="bg_img"></img>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
